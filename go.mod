211177b11645813f7c54fb1136aadc6b00335842
Mente <dev@imkreative.com>
2024-08-15 20:04:11 -0400

    jwt server udpate
    Add JWT to postGameto authenticate endpoint with which game to be
    updating
    Create JWT on game creation to access game

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cg219/common-game
 
 go 1.22.3
+
+require github.com/golang-jwt/jwt/v5 v5.2.1
--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/cg219/common-game/game"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type server struct {
@@ -16,6 +22,11 @@ type server struct {
 type gameResponse struct {
     Words []string `json:"words"`
     GameId int `json:"id"`
+    Token string `json:"token"`
+}
+
+type errorResponse struct {
+    Error string `json:"error"`
 }
 
 type moveResponse struct {
@@ -34,7 +45,21 @@ type storeData struct {
     sch <-chan game.StatusGroup
 }
 
+type ForwardRequestError struct {
+    Error error
+    ResponseWriter http.ResponseWriter
+    Request *http.Request
+    NextHandler http.Handler
+}
+
+type ContextKey int
+
 var store map[int]*storeData
+
+const (
+    GameId ContextKey = iota
+    Error
+)
 
 func newServer() *server {
     return &server{
@@ -48,25 +73,26 @@ func startServer() error {
 
     srv.mux.HandleFunc("GET /", getHome())
     srv.mux.HandleFunc("GET /game", getGame())
-    srv.mux.HandleFunc("POST /game/{id}", postGame())
+    srv.mux.Handle("POST /game", middlewareAuthGame(postGame()))
 
     return http.ListenAndServe(":3000", srv.mux)
 }
 
 func getHome() http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
+    return func(w http.ResponseWriter, _ *http.Request) {
         w.Header().Add("Content-Type", "text/plain")
         w.Write([]byte("Yay we're here!!"))
     }
 }
 
 func getGame() http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
+    return func(w http.ResponseWriter, _ *http.Request) {
         w.Header().Add("Content-Type", "application/json")
         game, err := game.Create()
 
         if err != nil {
-            panic(err)
+            w.Write(getErrResponse(err))
+            return
         }
 
         id := len(store)
@@ -78,46 +104,123 @@ func getGame() http.HandlerFunc {
             sch: statusCh,
         }
 
+        token := jwt.NewWithClaims(jwt.SigningMethodHS256, &jwt.RegisteredClaims{
+            Issuer: "common-game",
+            IssuedAt: jwt.NewNumericDate(time.Now()),
+            ExpiresAt: jwt.NewNumericDate(time.Now().Add(20 * time.Minute)),
+            Subject: fmt.Sprintf("%d", id),
+        })
+
+        stoken, err := token.SignedString([]byte("notsecure"))
+
+        if err != nil {
+            w.Write(getErrResponse(err))
+            return
+        }
+
         gr := &gameResponse{
             GameId: id,
             Words: game.Words(),
+            Token: stoken,
         }
 
         res, err := json.Marshal(gr)
 
         if err != nil {
-            panic(err)
+            w.Write(getErrResponse(err))
+            return
         }
 
         w.Write(res)
     }
 }
 
+func middlewareAuthGame(h http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        stoken := strings.Replace(r.Header.Get("Authorization"), "Bearer ", "", 1)
+        token, err := jwt.ParseWithClaims(stoken, &jwt.RegisteredClaims{}, func(t *jwt.Token) (interface{}, error) { return []byte("notsecure"), nil })
+
+        fre := &ForwardRequestError{
+            NextHandler: h,
+            ResponseWriter: w,
+            Request: r,
+        }
+
+        if err != nil {
+            fre.Error = err
+            forwardError(fre)
+            return
+        }
+
+        sid, err := token.Claims.GetSubject()
+
+        if err != nil {
+            fre.Error = err
+            forwardError(fre)
+            return
+        }
+
+        id, err := strconv.Atoi(sid)
+
+        if err != nil {
+            fre.Error = err
+            forwardError(fre)
+            return
+        }
+
+        ctx := context.WithValue(r.Context(), GameId, id)
+        r = r.WithContext(ctx)
+        h.ServeHTTP(w, r)
+    })
+}
+
+func forwardError(f *ForwardRequestError) {
+    log.Println(f.Error)
+    ctx := context.WithValue(f.Request.Context(), Error, errors.New("JWT Error"))
+    r := f.Request.WithContext(ctx)
+    f.NextHandler.ServeHTTP(f.ResponseWriter, r)
+}
+
+func getErrResponse(e error) []byte {
+    message := "Server Error: Something went wrong :c"
+    res := &errorResponse{ Error: message }
+    data, err := json.Marshal(res)
+    log.Println(e.Error())
+
+    if err != nil {
+        panic(nil)
+    }
+
+    return data
+}
+
 func postGame() http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         w.Header().Add("Content-Type", "application/json")
-        pv := r.PathValue("id")
-        id, err := strconv.Atoi(pv)
 
-        if err != nil {
-            w.Write([]byte("{\"status\": false}"))
+        cerr := r.Context().Value(Error)
+
+        if cerr, ok:= cerr.(error); ok {
+            w.Write(getErrResponse(error(cerr)))
             return
         }
+
+        id := r.Context().Value(GameId).(int)
 
         data, ok := store[id]
 
         if !ok {
-            w.Write([]byte("{\"status\": false}"))
+            w.Write(getErrResponse(fmt.Errorf("game with id %d not found", id)))
             return
         }
 
         body := &gamePost{}
-        err = json.NewDecoder(r.Body).Decode(body)
+        err := json.NewDecoder(r.Body).Decode(body)
 
         if err != nil {
-            w.Write([]byte("{\"status\": false}"))
+            w.Write(getErrResponse(err))
             return
-        } 
+        }
 
         data.mch <- game.Move{
             Words: body.Words,
@@ -134,7 +237,7 @@ func postGame() http.HandlerFunc {
         res, err := json.Marshal(moveRes)
 
         if err != nil {
-            w.Write([]byte("{\"status\": false}"))
+            w.Write(getErrResponse(err))
             return
         }
 
