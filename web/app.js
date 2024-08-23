9b8be06ab524ad2c58dfbca8958aabc6a6246146
Mente <dev@imkreative.com>
2024-08-23 19:28:38 -0400

    show game status + updated selection styles

--- web/app.js
+++ web/app.js
@@ -39,13 +39,19 @@ function reset(m) {
     m.forEach((_, i) => m[i] = undefined)
 }
 
+function removeWrong() {
+    htmx.findAll(".game-board button.wrong").forEach((b) => b.classList.remove("wrong"))
+}
+
 htmx.find(".game").addEventListener("click", function (evt) {
     const piece = evt.target.closest(".game-board button")
 
     if (piece && !piece.classList.contains("correct")) {
+        removeWrong()
         const word = piece.attributes.getNamedItem("data-value").value
 
         const selected = toggleSelection(moves, word)
+        piece.classList.toggle("selected")
         console.log(`Moves: ${moves}`)
 
         if (selected == 4) {
